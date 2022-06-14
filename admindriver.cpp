a304b25d6cf2ab3901d2e621048fe92e35773d8b
mariaclarin <maria.clarin@binus.ac.id>
2022-06-14 17:59:18 +0700

    debug total

--- admindriver.cpp
+++ admindriver.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include "queue.hpp"
 #include <string>
 using namespace std;
@@ -45,14 +46,15 @@ void displayMenu(){
     cout<<"11. Return"<<endl;
     cout<<"========================================================\n";
 }
-
+ 
 int main(){
     int adminChoice;
     string customerName;
     int orderNum;
     string customerOrder;
     string customerTotalTemp;
-    
+    double customerTotal = 0.00;
+
     queue<string> cusName(100);
     queue<string> cusOrder(100);
 
@@ -78,65 +80,65 @@ int main(){
                     switch(orderNum){
                         case 1:
                             customerOrder.append("Truffle Fries\n");
-                            customerTotal + 6.50;
+                            customerTotal += 6.50;
                             cout<<"One portion of TRUFFLE FRIES added!\n"<<endl;
                             break;
 
                         case 2:
                             customerOrder.append("Mac and Cheese Bites\n");
-                            customerTotal + 8.20;
+                            customerTotal += 8.20;
                             cout <<"One portion of MAC AND CHEESE BITES added!\n"<<endl;
                             break;
 
                         case 3:
                             customerOrder.append("Mozzarella Sticks\n");
-                            customerTotal + 9.40;
+                            customerTotal += 9.40;
                             cout<<"One portion of MOZZARELLA STICKS added!\n"<<endl;
                             break;
 
                         case 4:
                             customerOrder.append("Cocktail Meatballs\n");
-                            customerTotal + 6.50;
+                            customerTotal += 6.50;
                             cout<<"One portion of COCKTAIL MEATBALLS added!\n "<<endl;
                             break;
                         case 5:
                             // APPENDS TO ORDER STRING
                             customerOrder.append("Korean Fire Wings\n");
-                            customerTotal + 12.10;
+                            customerTotal += 12.10;
                             cout << "One portion of KOREAN FIRE WINGS added! \n"<< endl;
                             break;
 
                         case 6:
                             // APPENDS TO ORDER STRING
                             customerOrder.append("Smashed Cheese Burgers\n");
-                            customerTotal + 14.50;
+                            customerTotal += 14.50;
                             cout << "One portion of SMASHED CHEESE BURGERS added! \n" << endl;
                             break;
 
                         case 7:
                             // APPENDS TO ORDER STRING
                             customerOrder.append("Crispy Chicken Sandwich\n");
-                            customerTotal + 10.70;
+                            customerTotal += 10.70;
                             cout << "One portion of CRISPY CHICKEN SANDWICH added! \n" << endl;
                             break;
 
                         case 8:
                             // APPENDS TO ORDER STRING
                             customerOrder.append("Chicken Waffles\n");
-                            customerTotal + 10.70;
+                            customerTotal += 10.70;
                             cout << "One portion of CHICKEN WAFFLES added! \n"<< endl;
                             break;
                        case 9:
                             // APPENDS TO ORDER STRING
                             customerOrder.append("Coca Cola\n");
-                            customerTotal + 1.30;
+                            customerTotal += 1.30;
                             cout << "One bottle of COCA COLA added! \n" << endl;
                             break;
 
                         case 10:
                             // APPENDS TO ORDER STRING
                             customerOrder.append("Lemonade\n");
-                            customerTotal + 1.10;
+                            customerTotal += 1.10;
                             cout << "One bottle of LEMONADE added! \n" << endl;
                             break;
 
@@ -149,7 +151,6 @@ int main(){
                             break;
                     }
                 } while(orderNum !=11);
-                cout<< customerTotal<<endl;
                 customerTotalTemp = to_string(customerTotal);
                 customerOrder.append("\nTotal Price : $" + customerTotalTemp );
                 cusOrder.add(customerOrder);
